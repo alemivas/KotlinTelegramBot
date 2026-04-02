b310483c43d47b9110af0fc8d7cd29efaad1bfca
Alexander <a_36524@mail.ru>
2026-04-02 05:17:52 +0400

    KTB-05 Menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,25 @@ import java.io.File
 import java.io.FileNotFoundException
 
 fun main() {
+    println("=== Тренажер английских слов ===")
+    val dictionary = loadDictionary()
+
+    while (true) {
+        println()
+        println("Меню:")
+        println("1 – Учить слова")
+        println("2 – Статистика")
+        println("0 – Выход")
+        when (readln()) {
+            "1" -> println("Выбран пункт \"Учить слова\"")
+            "2" -> println("Выбран пункт \"Статистика\"")
+            "0" -> return
+            else -> println("Введите число 1, 2 или 0")
+        }
+    }
+}
+
+fun loadDictionary(): MutableList<Word> {
     val fileName = "words.txt"
     val wordsFile = File(fileName)
     var splitLine: List<String>
@@ -24,9 +43,7 @@ fun main() {
         println("Файл \"$fileName\" не найден")
     }
 
-    dictionary.forEach { word ->
-        println(word)
-    }
+    return dictionary
 }
 
 data class Word(
